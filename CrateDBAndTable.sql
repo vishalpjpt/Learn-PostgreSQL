d67dba1eede291c273723cb98db26edf02713911
Vishal Prajapati <vsp@aiwa.digital>
2024-05-16 19:34:08 +0530

    initial commit

---
+++ CrateDBAndTable.sql
@@ -0,0 +1,9 @@
+-- Create database query
+CREATE DATABASE shop;
+
+-- create products table
+CREATE TABLE products (
+    product_name VARCHAR(100),
+    quantity INTEGER,
+    price DECIMAL(10, 2)
+);
