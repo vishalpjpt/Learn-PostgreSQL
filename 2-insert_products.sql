6b15638c563b2f9a5121b39764055ebffda4acb9
Vishal Prajapati <vsp@aiwa.digital>
2024-05-17 14:51:14 +0530

    renamed files

---
+++ 2-insert_products.sql
@@ -0,0 +1,6 @@
+-- insert data
+insert into products (product_name, price, quantity) values ('Irish Cream - Butterscotch', 990.9, 14);
+
+insert into products (product_name, price, quantity) values ('Wine - Barossa Valley Estate', 891.61, 1);
+
+insert into products (product_name, price, quantity) values ('Squeeze Bottle', 468.87, 35);
