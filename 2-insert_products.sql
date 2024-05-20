ec6bf56272bb83b2de7feea080966bb93625508c
Vishal Prajapati <vsp@aiwa.digital>
2024-05-20 12:00:21 +0530

    added insery query syntax

--- 2-insert_products.sql
+++ 2-insert_products.sql
@@ -1,3 +1,11 @@
+
+-- insert query syntax
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES
+(value1a, value2a, value3a, ...),
+(value1b, value2b, value3b, ...),
+...;
+
 -- insert data
 insert into products (product_name, price, quantity) values ('Irish Cream - Butterscotch', 990.9, 14);
 
